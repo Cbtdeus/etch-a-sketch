b8924e0d36311a9d2b8b2cb18b0ad3b9ac3c439f
Cbtdeus <cbtdeus@proton.me>
2024-09-10 09:30:57 +0300

    added canvas sizing

--- javascript.js
+++ javascript.js
@@ -1,13 +1,33 @@
 const body = document.querySelector("body");
-const etch = document.querySelector(".etch");
-gridSize = 16;
-for (let i = 0;i < (gridSize * gridSize) ;i++ ){
+const button = document.querySelector("button")
+const etch = document.querySelector(".etch")
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+function createCanvas(gridSize = 32){
+    removeAllChildNodes(etch)
+     for (let i = 0;i < (gridSize * gridSize) ;i++ ){
     const div = document.createElement("div")
     div.addEventListener("mouseover", () => {
         div.style.backgroundColor = "red"})
+    div.setAttribute("class", "sketch")
     etch.appendChild(div)
-   
-    };
+    div.style.flexBasis = (100 / gridSize) + "%" ;
+    }};
+createCanvas()
+
+button.addEventListener("click", () =>{
+    gridSize = prompt("Grid size:")
+    if (gridSize > 100){
+        alert("Invalid grid size")
+        createCanvas()
+        return
+    }
+    createCanvas(gridSize)
+})
     
+
  /*    const div = document.createElement("div")
     etch.appendChild(div) */
